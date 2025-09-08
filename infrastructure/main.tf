9dd381eb89253300b6f3dae6b0cdbee694442749
vishpatel913 <vishpatel913@googlemail.com>
2025-09-08 13:00:51 +0100

    fixing plan

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,26 +19,30 @@ resource "aws_ecr_repository" "api" {
   image_scanning_configuration {
     scan_on_push = true
   }
-
-  lifecycle_policy {
-    policy = jsonencode({
-      rules = [
-        {
-          rulePriority = 1
-          description  = "Keep last 3 images"
-          selection = {
-            tagStatus   = "any"
-            countType   = "imageCountMoreThan"
-            countNumber = 3
-          }
-          action = {
-            type = "expire"
-          }
-        }
-      ]
-    })
-  }
 }
+
+# ECR Lifecycle Policy
+resource "aws_ecr_lifecycle_policy" "api" {
+  repository = aws_ecr_repository.api.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 3 images"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 3
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
+
 
 # Lambda execution role
 resource "aws_iam_role" "lambda_role" {
