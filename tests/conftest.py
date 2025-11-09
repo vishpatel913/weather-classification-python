f0dea3c86e25226aebdf68c6025d0db908f0d7ce
vishpatel913 <vishpatel913@googlemail.com>
2025-11-09 12:36:18 +0000

    testing regex for mock route

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,8 +20,9 @@ def fixtures_weather_api_mock():
     with respx.mock(
         base_url=settings.weather_api_base_url, assert_all_called=False
     ) as httpx_mock:
-        forecast_route = httpx_mock.get(
-            path="/forecast",
+        forecast_route = httpx_mock.route(
+            method="GET",
+            url__regex=r".*/forecast.*",
             name="forecast",
         )
         forecast_route.return_value = Response(200, json=[])
